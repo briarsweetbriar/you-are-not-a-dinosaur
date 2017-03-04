d355748dc173697e1cd064a0d3cfff5fab071ab3
null null null <null@affinitygamescollective.org>
2017-03-04 09:04:48 -0800

    add cc

--- app/affinity-engine/stage/scenes/initial.js
+++ app/affinity-engine/stage/scenes/initial.js
@@ -4,7 +4,8 @@ import { task } from 'ember-concurrency';
 export default Scene.extend({
   start: task(function * (script) {
     yield script.pause(500);
-    yield script.text("Please be advised that this game features insistent misrecognition. It's upsetting enough in real life, so you might not want to play this gamified version of it.");
+    yield script.text("Please be advised that this game is about insistent misrecognition. It's shitty enough in real life, so you might not want to play this gamified version of it.");
+    yield script.text("Also, this game has ♭♩♪♬, so you might want to mute your speakers if you're at work or something.")
 
     script.scene('main');
   })
