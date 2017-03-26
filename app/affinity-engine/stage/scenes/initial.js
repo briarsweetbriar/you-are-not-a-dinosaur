fc002b8fa7cdb971f7ae1c8e87f08a34c03151ce
null null null <null@affinitygamescollective.org>
2017-03-25 21:18:12 -0700

    tweak inital text

--- app/affinity-engine/stage/scenes/initial.js
+++ app/affinity-engine/stage/scenes/initial.js
@@ -4,8 +4,8 @@ import { task } from 'ember-concurrency';
 export default Scene.extend({
   start: task(function * (script) {
     yield script.pause(500);
-    yield script.text("Heads up, this game is about insistent misrecognition: aka, the shitty experience of having someone insist that you are something you are not.");
-    yield script.text("Also, this game has amazing ♭♩♪♬ (!!!?), so you might want to mute your speakers if you're at work or something.")
+    yield script.text("Heads up, this game explores insistent misrecognition: that is, having someone insist that you are something you are not.");
+    yield script.text("Also, this game has ♭♩♪♬, so you might want to mute your speakers if you're at work or something.")
 
     script.scene('main');
   })
